5e34ffafbedf20926df1a018ab025a6adc1d6751
Marlen <meaguilar@uca.edu.sv>
2026-04-15 18:28:06 -0600

    Eliminar carpeta modelo y sus clases (Atleta, Nadador)

--- EjerciciosClases/GestionAtleta/src/main/java/modelo/Deporte.java
+++
@@ -1,9 +0,0 @@
-package modelo;
-
-public enum Deporte {
-    NATACION,
-    FUTBOL,
-    VOLLEYBALL,
-    BOXEO,
-    BALONCESTO
-}
--- EjerciciosClases/GestionAtleta/src/main/java/modelo/Imprimible.java
+++
@@ -1,5 +0,0 @@
-package modelo;
-
-public interface Imprimible {
-    public void imprimir();
-}
