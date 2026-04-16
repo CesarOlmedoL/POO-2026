a52a3337e71f23dbdc1c95f4d3ab8b080ec3c095
Marlen <meaguilar@uca.edu.sv>
2026-04-16 16:55:23 -0600

    feat: modificar los datos de prueba

--- EjerciciosClases/GestionAtleta/src/main/java/org/example/Main.java
+++ EjerciciosClases/GestionAtleta/src/main/java/org/example/Main.java
@@ -13,27 +13,36 @@ public class Main {
                 0.0, 0.0, "Mariposa");
 
         nadador1.imprimir();
-        nadador1.setImc(nadador1.calcularIMC());
+        // estableciendo el promHorasCalculadas
         nadador1.setPromHorasEntrenamiento(nadador1.calcularPromedioSemanalEntrenamiento());
         nadador1.cambiarEstilo("Libre (Crol)");
+        // estableciendo el imc calculado
+        nadador1.setImc(nadador1.calcularIMC());
         System.out.print("Clasificación del IMC: ");
         nadador1.clasificarIMC();
         System.out.print("Clasificación del rendimiento: ");
         nadador1.clasificarRendimiento();
         nadador1.entrenar();
 
-        // Creando el equipo
-        Equipo equipo1 = new Equipo();
-        equipo1.setNombre("Equipo mundial1");
-        equipo1.agregarMiembrosEquipo(nadador1);
+        // Crear el equipo
 
 
-        // modelo.Entrenador
+        System.out.println("\nDatos del entrenador: ");
+        // Crear al entrenador
         Entrenador entrenador1 = new Entrenador("Julio", 4);
-        System.out.print("Salario del entrenador: " + entrenador1.calcularSalario());
-        entrenador1.asignarAtleta(nadador1);
+        System.out.println("Salario del entrenador $: " + entrenador1.calcularSalario());
         entrenador1.imprimir();
 
+
+        // Determinar el objetivo del plan de entrenamiento
+
+        // Crear el plan de entrenamiento
+
+        // Agregar los datos del plan de entrenamiento, objetivos y ejercicios
+
+        // preescribir plan de entrenamiento al atleta
+
+        // Imprimiendo el plan de entrenamiento asignado a un atleta
 
     }
 }
